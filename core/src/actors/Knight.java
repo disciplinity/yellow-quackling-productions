0a2984daf1c2819f233ab487fecad21404f70dea
Artur Bassov <artbas@ttu.ee>
2018-02-21 11:28:21 +0200

    Merge branch 'master' of https://github.com/disciplinity/yellow-quackling-productions
    # Conflicts:
    #	gradle/wrapper/gradle-wrapper.properties

--- core/src/actors/Knight.java
+++ core/src/actors/Knight.java
@@ -3,6 +3,8 @@ package actors;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Knight extends Actor {
+    private String name;
+
     Knight() {
         System.out.println("lol");
     }
