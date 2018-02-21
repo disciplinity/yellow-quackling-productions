4a6032cc14b80f7e91adb3815c33a9fe26c754d1
jagors <jagors@ttu.ee>
2018-02-21 11:29:18 +0200

    git test

--- core/src/actors/Knight.java
+++ core/src/actors/Knight.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Knight extends Actor {
     private String name;
+    private String lol;
 
     Knight() {
         System.out.println("lol");
