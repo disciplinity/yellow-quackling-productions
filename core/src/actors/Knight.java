bf9191148d16c08a3cff88493969bd49d8e9b153
nikasamuell <zamakhovav@gmail.com>
2018-02-21 11:29:44 +0200

    by veronika

--- core/src/actors/Knight.java
+++ core/src/actors/Knight.java
@@ -3,7 +3,10 @@ package actors;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Knight extends Actor {
+    public final int WIDTH = 10;
+
     private String name;
+
 
     Knight() {
         System.out.println("lol");
