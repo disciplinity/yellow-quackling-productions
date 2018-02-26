cf64b128a30fbdc60b6e2091abbb0a37d71870c5
Artur Bassov <artbas@ttu.ee>
2018-02-27 00:52:41 +0200

    Merge commit

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,13 +3,16 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
+import network.kryonet.ui_frame.MyUITest;
+import network.kryonet.ui_frame.UISimpleTest;
+import network.kryonet.ui_frame.UITest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Zen";
-		config.width = 1280;
-        config.height = 1245;
-		new LwjglApplication(new MyGdxGame(), config);
+		config.width = 800;
+        config.height = 600;
+		new LwjglApplication(new MyUITest(), config);
 	}
 }
