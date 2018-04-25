d5d37d0443a1106124abf8c88ef531e743a41547
Artur Bassov <artbas@ttu.ee>
2018-04-25 11:00:43 +0300

    Merge branch 'master' of https://github.com/disciplinity/yellow-quackling-productions
    # Conflicts:
    #	core/src/main/java/game/MyGdxGame.java
    #	core/src/main/java/game/screens/StartMenuScreen.java

--- core/src/main/java/network/database/HibernateSessionFactory.java
+++ core/src/main/java/network/database/HibernateSessionFactory.java
@@ -10,16 +10,18 @@ public class HibernateSessionFactory {
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     protected static SessionFactory buildSessionFactory() {
-        // A SessionFactory is set up once for an application!
+        /* A SessionFactory is set up once for an application! */
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
+                .configure() /* configures settings from hibernate.cfg.xml  */
                 .build();
         try {
             sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
         }
         catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
+            /*
+            The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            so destroy it manually.
+            */
             StandardServiceRegistryBuilder.destroy( registry );
 
             throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
